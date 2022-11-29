32cdd2df80005ed7794c9898014c0654095923d4
Dorian Schandené <dschandene@gmail.com>
2022-11-29 13:54:31 +0100

    fin de l'exercice 1

---
+++ ex01/ft_print_alphabet.js
@@ -0,0 +1,11 @@
+
+function alphabet_order(){
+    const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    let resultat="";
+    
+    for(var i=0; i<alphabet.length; i++){
+        resultat = resultat+" "+alphabet[i];
+    }
+
+    console.log(resultat);
+}
