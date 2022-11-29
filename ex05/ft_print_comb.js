3ac929cc86faa4512ee1e5b862a794b7e4a8db92
Dorian Schandené <dschandene@gmail.com>
2022-11-29 19:35:14 +0100

    Fin de l'exercice 5

--- ex05/ft_print_comb.js
+++ ex05/ft_print_comb.js
@@ -2,7 +2,8 @@ function comb(){
 
     //Première étape : Générer des compositions sans doublon, exemple : 123 et non 111;
 
-    let traitement = []; //initialisation du tableau final de donnée
+    let traitement = []; //initialisation du tableau de traitement de donnée
+    let result = []; //initialisation du tableau final de traitement
    
     for(var i=0; i<=987; i++){ //traitement sur 987 étant la valeur maximale possible
   
@@ -15,26 +16,35 @@ function comb(){
         }else if(i<1000){
             value=""+i;
         }
-    
-        //console.log(value);
 
-        let array = value.split(''); //on split la valeur. Exemple : 123 = [1,2,3]
+        let value_split = value.split(''); //on split la valeur. Exemple : 123 = array[1,2,3]
          
 
-        if(array[0]==array[1] || array[0]==array[2] || array[1]==array[2]){
-            //Si 2 chiffres de la composition sont égaux, alors on ne fait rien, sinon on récupère la valeur et on push dans l'array final
+        if(value_split[0]==value_split[1] || value_split[0]==value_split[2] || value_split[1]==value_split[2]){
+        //Si 2 chiffres de la composition sont égaux, alors on ne fait rien, sinon on récupère la valeur et on push dans l'array de traitement
 
             //console.log("min 2 chiffres sont identiques");
+
         }else{
+
             //console.log("tous les chiffres sont différents");
-            traitement.push(value);
-        }
+            
+            if(value_split[0] < value_split[1] && value_split[1] < value_split[2]) {
+
+            //Donc si les chiffres qui composent un nombre sont rangés dans un ordre CROISSANT, 
+            //alors il n'existe aucun nombre ayant la même combinaison de chiffres qui soit inférieur.
+            //Les autres possibilités futures seront donc exclues. Exemple : 014 = ok ; 041 = exclu ; 123 = ok ; 132 = exclu;
+
+                traitement.push(value); //On ajoute la valeur filtrée dans le tableau post traitement
+
+            }
+
+
+       }
 
     }
 
-
-
+    
     return traitement;
-
 
 }
